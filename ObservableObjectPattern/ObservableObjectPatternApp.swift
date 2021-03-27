00bb946c6fb4509fc4d3c6c0101b9f5a1a69853c
Rick Brown <ricky.brown.00@gmail.com>
2021-03-27 13:07:40 +0000

    Initial Commit

---
+++ ObservableObjectPattern/ObservableObjectPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableObjectPatternApp.swift
+//  ObservableObjectPattern
+//
+//  Created by Rick Brown on 27/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ObservableObjectPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
