97449136aed11b7b760eed25ec9ed128c3b91437
Rick Brown <ricky.brown.00@gmail.com>
2021-03-31 14:11:20 +0100

    adds documentation

--- ObservableObjectPattern/ViewModel.swift
+++ ObservableObjectPattern/ViewModel.swift
@@ -7,24 +7,37 @@
 
 import Foundation
 
+
+/**
+ A view model to represent the `Counter` model
+ 
+ Available public variables:
+ - `counter` - An integer to represent the counter.
+ - `currentName` - A string which repsents the user at the counter index of the counter models `names` array.
+ - `increment(by value: Int)` - A function to increment the counter in the counter model.
+ - `decrement(by value: Int)` - A function to decrement the counter in the counter model.
+*/
 class ViewModel: ObservableObject {
   @Published var counterModel = Counter()
   
+  /// The current counter. Will be used in generating the `currentName` variable
   var counter: Int {
     counterModel.counter
   }
   
+  /// The name of the user located at the current counters index in the names array (see Counter.swift)
   var currentName: String {
-    if counterModel.counter <= 2 && counterModel.counter >= 0 {
-      return counterModel.names[counterModel.counter]
-    }
-    return "no more names"
+    counterModel.counter <= 2 && counterModel.counter >= 0 ? counterModel.names[counterModel.counter] : "no more names"
   }
   
+  /// A method to add an amount to the current counter total
+  /// - Parameter by: A value with which to add to the existing counter total.
   func increment(by value: Int) {
     counterModel.increment(by: value)
   }
   
+  /// A method to subract an amount from the current counter total.
+  /// - Parameter by: A value with which to subtract from the existing counter total.
   func decrement(by value: Int) {
     counterModel.decrement(by: value)
   }
--- ObservableObjectPatternTests/ObservableObjectPatternTests.swift
+++ ObservableObjectPatternTests/ObservableObjectPatternTests.swift
@@ -8,7 +8,17 @@
 import XCTest
 @testable import ObservableObjectPattern
 
+/**
+ The test method should contain three steps, in the order described below:
+   `Arrange`
+    Create any objects or data structures that the code path you’re exercising uses. Replace complex dependencies with “stubs” that are easy to configure, to ensure that tests run quickly and are deterministic. Adopting protocol-oriented programming ensures that relationships between objects in your app are sufficiently flexible to enable substitution of real implementations for stubs.
+   `Act`:
+    Call the method or function that you’re testing, using parameters and properties that you configured in the Arrange phase.
+   `Assert`:
+    Use Test Assertions to compare the behavior of the code exercised in the Act phase with your expectations of what should happen. Any assertion whose condition is false causes a test to fail.
+ */
 class ObservableObjectPatternTests: XCTestCase {
+  // Arange
   var viewModel: ViewModel!
   
   override func setUpWithError() throws {
@@ -21,7 +31,9 @@ class ObservableObjectPatternTests: XCTestCase {
   
   func testIncrementAndDecrementFunctions() throws {
     XCTAssertEqual(viewModel.counter, .zero)
+    // Act
     viewModel.increment(by: 5)
+    // Assert
     XCTAssertEqual(viewModel.counter, 5)
     viewModel.decrement(by: 5)
     XCTAssertEqual(viewModel.counter, .zero)
